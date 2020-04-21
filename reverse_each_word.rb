98e86b5d8b7fca26a35acd11da925fa1cb3b9d29
Chris Hosler <cmstie@gmail.com>
2020-04-21 23:11:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 require 'pry'
 def reverse_each_word(str)
   array = str.split(' ')
-  array.each do |x|
-    x.reverse!
+  array.each do |element|
+    element.reverse!
   end
   array.join(" ")
   
